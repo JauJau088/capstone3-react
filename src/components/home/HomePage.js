bedd8d29a55332a45dc7ccbf1f4cc68cfd8bb469
jaujau088 <jaujau088@gmail.com>
2022-06-21 17:29:30 +0200

    Add and setup redux & API

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,7 +1,22 @@
-const HomePage = () => (
-  <div>
-    Home
-  </div>
-)
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchJobs } from '../../redux/jobs/jobs';
+
+const HomePage = () => {
+  const data = useSelector((state) => state.jobsReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchJobs('frontend')), []);
+  console.log(data);
+
+  return (
+    <div>
+      <div>Home</div>
+      <div>
+        State =
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
